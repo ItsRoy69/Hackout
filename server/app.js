b5e6d6e02951e3fb4beb5926b1a43e6393d0a5ca
Sayak Sengupta <sayaksengupta74@gmail.com>
2022-10-15 01:14:45 +0530

    Added Folder Structure and Connected DB

---
+++ server/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("./db/conn");
+app.use(cookieParser());
+const connection = require("./db/conn");
+app.use(express.urlencoded({ extended: false }));
+require("./routes/startup/routes")(app);
+const port = process.env.PORT || 8000;
+
+
+
+connection();
+
+
+
+const server = http.createServer(app);
+
+
+
+server.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
---
+++ server/routes/startup/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+var indexRouter = require('../index');
+
+
+module.exports = function(app) {
+    app.use(express.json());
+  
+    app.use("/", indexRouter);
+  };
