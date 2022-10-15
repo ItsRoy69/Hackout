bc09e6751311aaec036ba77fd12d5f0b271cc941
Sayak Sengupta <sayaksengupta74@gmail.com>
2022-10-15 15:09:09 +0530

    Minor Fixes

--- server/routes/startup/routes.js
+++ server/routes/startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 var indexRouter = require('../index');
+var customerRouter = require('../customer');
 
 
 module.exports = function(app) {
     app.use(express.json());
   
     app.use("/", indexRouter);
+    app.use('/customers', customerRouter);
   };
