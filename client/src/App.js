6be294844fd84fe502b6c6dda8b03d0804aaa02e
Tamal Das <gyansujan69@gmail.com>
2022-10-15 10:39:53 +0530

    feat : Added User's dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import WorkerRegistration from "./pages/WorkerRegistration"
-import WorkerLogin from './pages/WorkerLogin';
-import ShopRegister from './pages/ShopRegister';
-import ShopLogin from './pages/ShopLogin';
-import ServiceLogin from './pages/ServiceLogin';
-import ServiceSignUp from './pages/ServiceSignUp';
+import WorkerRegistration from "./pages/WorkerRegistration";
+import WorkerLogin from "./pages/WorkerLogin";
+import ShopRegister from "./pages/ShopRegister";
+import ShopLogin from "./pages/ShopLogin";
+import ServiceLogin from "./pages/ServiceLogin";
+import ServiceSignUp from "./pages/ServiceSignUp";
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/workerregistration" exact element={<WorkerRegistration />} />
-            <Route path="/workerlogin" exact element={<WorkerLogin />} />
-            <Route path="/shopregister" exact element={<ShopRegister />} />
-            <Route path="/shoplogin" exact element={<ShopLogin />} />
-            <Route path="/servicelogin" exact element={<ServiceLogin />} />
-            <Route path="/servicesignup" exact element={<ServiceSignUp />} />
-          </Routes>
-        </div>
-      </Router> 
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route
+            path="/workerregistration"
+            exact
+            element={<WorkerRegistration />}
+          />
+          <Route path="/workerlogin" exact element={<WorkerLogin />} />
+          <Route path="/shopregister" exact element={<ShopRegister />} />
+          <Route path="/shoplogin" exact element={<ShopLogin />} />
+          <Route path="/servicelogin" exact element={<ServiceLogin />} />
+          <Route path="/servicesignup" exact element={<ServiceSignUp />} />
+          <Route path="/userdashboard" exact element={<UserDashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
